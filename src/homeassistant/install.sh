2bbcacfad3d6fa3a1f2f12ce65bc690947851037
ludeeus <joasoe@gmail.com>
2023-01-27 20:48:26 +0000

    Add guards

--- src/homeassistant/install.sh
+++ src/homeassistant/install.sh
@@ -3,6 +3,18 @@ set -e
 
 VERSION=${VERSION:-"stable"}
 
+if ! command -v python3 &> /dev/null
+then
+    echo "python3 could not be found"
+    exit
+fi
+
+if ! command -v $(python3 -m pip) &> /dev/null
+then
+    echo "pip could not be found"
+    exit
+fi
+
 if [ "${VERSION}" == "stable" ] || [ -z "${VERSION}" ]; then
     python3 -m pip install homeassistant
 elif [ "${VERSION}" == "beta" ]; then
